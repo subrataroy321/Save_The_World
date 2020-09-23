624195821374a59f62abd14bacab426ee7740806
subrataroy321 <subrata.r321@gmail.com>
2020-09-23 17:56:22 -0400

    add styling to App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,43 @@ import React from 'react';
 import './App.css';
 import {Link} from 'react-router-dom'
 import './State';
+import logo from './assets/logo.png'
 
 
 
 function App(props) {
   return (
-    <div>
+    <div className='App'>
       <header>
-        <h1>Save the World!</h1>
+      <nav>
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="/">Home</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
       </header>
-      <h2>About Us</h2>
-      <p>Here to help you help us save the world.</p>
-      <h2>States</h2>
-      <ul>
-        {props.states.map((state, i) => {
-          return <li><Link key={i} to={{pathname: `/${state.name}`, state: state}}>{state.name}</Link></li>
-        })}
-      </ul>
+      <div>
+        <div>
+          <img className="logo-image" src={logo} alt="logo"/>
+        </div>
+        <h2>About Us</h2>
+        <p>Here to help you help us save the world.</p>
+        <h2>Select Your State</h2>
+        <div className="allStatesDiv">
+          {props.states.map((state, i) => {
+            return (
+              <Link key={i} to={{pathname: `/${state.name}`, state: state}}>
+                <div className="stateDiv">
+                  {state.name}
+                </div>
+              </Link>
+              )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
