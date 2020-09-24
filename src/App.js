107e2d6e709bfebb0a847be94d939ae33baef7c8
subrataroy321 <subrata.r321@gmail.com>
2020-09-24 01:53:58 -0400

    edit navbar add dropdown state add about component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import React from 'react';
 import './App.css';
 import {Link} from 'react-router-dom'
 import './State';
+import usMap from './assets/us-map.png'
 
 function App(props) {
   return (
     <div className='App'>
       <div>
-        <h2>About Us</h2>
+        <br/>
+        <br/>
+        <br/>
         <p>Here to help you help us save the world.</p>
         <h2>Select Your State</h2>
         <div className="allStatesDiv">
           {props.states.map((state, i) => {
             return (
               <Link key={i} to={{pathname: `/${state.name}`, state: state}}>
-                <div className="stateDiv" style={{ backgroundImage: `url(https://cdn.civil.services/us-states/maps/${state.name.toLowerCase()}-large.png)`}}>
+                {
+                !state.map_image_url
+                ?
+                <div className="stateDiv" style={{ backgroundImage: `url(${usMap})`}}>
                   {state.name}
                 </div>
+                :
+                <div className="stateDiv" style={{ backgroundImage: `url(${state.map_image_url})`}}>
+                  {state.name}
+                </div>
+                }
               </Link>
               )
           })}
--- src/State.js
+++ src/State.js
@@ -23,8 +23,7 @@ function State(props){
 
     return(
 
-        <div>
-            <h1>This is our nav bar</h1>
+        <div className="state-details">
             <div className="carousel">
                 <a className="carousel-item" href="#one!"><img src={props.location.state.landscape_background_url} alt="landscape" /></a>
                 <a className="carousel-item" href="#two!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
@@ -33,49 +32,51 @@ function State(props){
                 <a className="carousel-item" href="#five!"><img src={props.location.state.landscape_background_url} alt="landscape" /></a>
                 <a className="carousel-item" href="#six!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
             </div>
-
-        <div className="state-details">
-      
-            <h1>{props.location.state.name}</h1>
-            <p>{props.location.state.info}</p>
-            <h1>Here are some actions you can take</h1>
-            
-            <ul>
-                {props.location.state.actions  
-                    ? props.location.state.actions.map((action, i) =>{
-                        return <li key={i}>{action}</li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
-            
-            <h1>Where you can donate</h1>
-            
-            <ul>
-            {props.location.state.nonprofits 
-                    ? props.location.state.nonprofits.map((org, i) =>{
-                        return <li key={i}><a href = {org.website} target="_blank" rel="noopener noreferrer">{org.name}</a></li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
-            
-            <h1>Volunteer</h1>
-            
-            <ul>
-            {props.location.state.volunteer 
-                    ? props.location.state.volunteer.map((day, i) =>{
-                        return <li key={i}>{day}</li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
         
-            <h1>Local Initiatives</h1>
-            <ul>
-            {props.location.state.localPrograms 
-                    ? props.location.state.localPrograms.map((program, i) =>{
-                        return <li key={i}><a href = {program.website} target="_blank" rel="noopener noreferrer">{program.name}</a></li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
+
+            <div >
+      
+                <h1>{props.location.state.name}</h1>
+                <p>{props.location.state.info}</p>
+                <h1>Here are some actions you can take</h1>
+                
+                <ul>
+                    {props.location.state.actions  
+                        ? props.location.state.actions.map((action, i) =>{
+                            return <li key={i}>{action}</li>})
+                            : <p>Coming Soon...</p>}
+                </ul>
+                
+                <h1>Where you can donate</h1>
+                
+                <ul>
+                {props.location.state.nonprofits 
+                        ? props.location.state.nonprofits.map((org, i) =>{
+                            return <li key={i}><a href = {org.website} target="_blank" rel="noopener noreferrer">{org.name}</a></li>})
+                            : <p>Coming Soon...</p>}
+                </ul>
+                
+                <h1>Volunteer</h1>
+                
+                <ul>
+                {props.location.state.volunteer 
+                        ? props.location.state.volunteer.map((day, i) =>{
+                            return <li key={i}>{day}</li>})
+                            : <p>Coming Soon...</p>}
+                </ul>
+            
+                <h1>Local Initiatives</h1>
+                <ul>
+                {props.location.state.localPrograms 
+                        ? props.location.state.localPrograms.map((program, i) =>{
+                            return <li key={i}><a href = {program.website} target="_blank" rel="noopener noreferrer">{program.name}</a></li>})
+                            : <p>Coming Soon...</p>}
+                </ul>
+            </div>
         </div>
 
     )
 }
 
 
-export default State
+export default State;
