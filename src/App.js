f1b54a195cfca3a67148762cfe4be3810e387b05
Elaine <elaine@Felipes-iMac.local>
2020-09-24 10:55:05 -0500

    add info to app.js, add scrollspy to state.js, add readme

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App(props) {
       <div>
         <h2>About Us</h2>
         <p>Here to help you help us save the world.</p>
+        <p>With each day presents new challenges to our environment, making the impact of climate change felt around the globe. While solutions are needed at every level to produce a permanent response, Save The World provides an opportunity to connect with the organizations in your state working towards local solutions. We hope you'll join us to be informed, make a difference, and support others in their work. For us, for our future.</p>
         <h2>Select Your State</h2>
         <div className="allStatesDiv">
           {props.states.map((state, i) => {
--- src/State.js
+++ src/State.js
@@ -18,13 +18,24 @@ function State(props){
             duration: 10000
             }) 
         instance.next()
+        const elems = document.querySelectorAll(".scrollspy");
+        M.ScrollSpy.init(elems, { scrollOffset: 0 });
     })
     
 
     return(
 
         <div>
-            <h1>This is our nav bar</h1>
+            
+            <div class="col hide-on-small-only m3 l2">
+            <ul class="section table-of-contents">
+                <li><a href="#info">Info</a></li>
+                <li><a href="#actions">Actions</a></li>
+                <li><a href="#donation">Donations</a></li>
+                <li><a href="#volunteer">Volunteer Opportunities</a></li>
+            </ul>
+            </div>
+        
             <div className="carousel">
                 <a className="carousel-item" href="#one!"><img src={props.location.state.landscape_background_url} alt="landscape" /></a>
                 <a className="carousel-item" href="#two!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
@@ -34,45 +45,55 @@ function State(props){
                 <a className="carousel-item" href="#six!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
             </div>
 
-        <div className="state-details">
-      
-            <h1>{props.location.state.name}</h1>
-            <p>{props.location.state.info}</p>
-            <h1>Here are some actions you can take</h1>
-            
-            <ul>
-                {props.location.state.actions  
-                    ? props.location.state.actions.map((action, i) =>{
-                        return <li key={i}>{action}</li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
-            
-            <h1>Where you can donate</h1>
-            
-            <ul>
-            {props.location.state.nonprofits 
-                    ? props.location.state.nonprofits.map((org, i) =>{
-                        return <li key={i}><a href = {org.website} target="_blank" rel="noopener noreferrer">{org.name}</a></li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
-            
-            <h1>Volunteer</h1>
-            
-            <ul>
-            {props.location.state.volunteer 
-                    ? props.location.state.volunteer.map((day, i) =>{
-                        return <li key={i}>{day}</li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
-        
-            <h1>Local Initiatives</h1>
-            <ul>
-            {props.location.state.localPrograms 
-                    ? props.location.state.localPrograms.map((program, i) =>{
-                        return <li key={i}><a href = {program.website} target="_blank" rel="noopener noreferrer">{program.name}</a></li>})
-                        : <p>Coming Soon...</p>}
-            </ul>
-        </div>
+            <div class="row">
+                <div class="col s12 m9 l10">
+
+                    <div id="info" class="section scrollspy">
+                        <h1>{props.location.state.name}</h1>
+                        <p>{props.location.state.info}</p> 
+                    </div>
+
+                    <div id="actions" class="section scrollspy">
+                        <h1>Here are some actions you can take</h1>
+                
+                        <ul>
+                            {props.location.state.actions  
+                                ? props.location.state.actions.map((action, i) =>{
+                                    return <li key={i}>{action}</li>})
+                                    : <p>Coming Soon...</p>}
+                        </ul>
+                    </div>
+                    <div id="donation" class="section scrollspy"></div>
+                        <h1>Where you can donate</h1>
+                    
+                        <ul>
+                        {props.location.state.nonprofits 
+                                ? props.location.state.nonprofits.map((org, i) =>{
+                                    return <li key={i}><a href = {org.website} target="_blank" rel="noopener noreferrer">{org.name}</a></li>})
+                                    : <p>Coming Soon...</p>}
+                        </ul>
+                    </div>
+                    <div id="volunteer" class="section scrollspy">
+                        <h1>Volunteer</h1>
+                        
+                        <ul>
+                        {props.location.state.volunteer 
+                                ? props.location.state.volunteer.map((day, i) =>{
+                                    return <li key={i}>{day}</li>})
+                                    : <p>Coming Soon...</p>}
+                        </ul>
+                    </div>
+
+                </div>
+
+                <h1>Local Initiatives</h1>
+                <ul>
+                {props.location.state.localPrograms 
+                        ? props.location.state.localPrograms.map((program, i) =>{
+                            return <li key={i}><a href = {program.website} target="_blank" rel="noopener noreferrer">{program.name}</a></li>})
+                            : <p>Coming Soon...</p>}
+                </ul>
+            </div>
 
     )
 }
