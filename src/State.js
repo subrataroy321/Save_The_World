340f16d0017b52baae78c83b9db8d432503260a4
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-09-23 20:50:04 -0600

    add carosel to state.js

--- src/State.js
+++ src/State.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect} from 'react'
+import './App.css'
+import M from "materialize-css"
 // import {Link} from 'react-router-dom'
 
 function State(props){
     console.log(props.location.state.nonprofits)
+    useEffect(() => {
+        let elem = document.querySelector(".carousel");
+        console.log(elem)
+        let instance = M.Carousel.init(elem, {
+            fullWidth: true,
+            indicators: true,
+            numVisible: 1,
+            duration: 10000
+            }) 
+        instance.next()
+    })
+    
+
     return(
         <div>
             <h1>This is our nav bar</h1>
-
-           <h1>{props.location.state.name}</h1>
+            <div className="carousel">
+                <a className="carousel-item" href="#one!"><img src={props.location.state.landscape_background_url} alt="landscape" /></a>
+                <a className="carousel-item" href="#two!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
+                <a className="carousel-item" href="#three!"><img src={props.location.state.landscape_background_url} alt="landscape" /></a>
+                <a className="carousel-item" href="#four!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
+                <a className="carousel-item" href="#five!"><img src={props.location.state.landscape_background_url} alt="landscape" /></a>
+                <a className="carousel-item" href="#six!"><img src={props.location.state.skyline_background_url} alt="skyline" /></a>
+            </div>
+            <h1>{props.location.state.name}</h1>
             <p>{props.location.state.info}</p>
             <h1>Here are some actions you can take</h1>
             
             <ul>
                 {props.location.state.actions  
-                    ? props.location.state.actions.map(action =>{
-                        return <li>{action}</li>})
+                    ? props.location.state.actions.map((action, i) =>{
+                        return <li key={i}>{action}</li>})
                         : <p>Coming Soon...</p>}
             </ul>
             
@@ -23,8 +44,8 @@ function State(props){
             
             <ul>
             {props.location.state.nonprofits 
-                    ? props.location.state.nonprofits.map(org =>{
-                        return <li><a href = {org.website} target="_blank">{org.name}</a></li>})
+                    ? props.location.state.nonprofits.map((org, i) =>{
+                        return <li key={i}><a href = {org.website} target="_blank" rel="noopener noreferrer">{org.name}</a></li>})
                         : <p>Coming Soon...</p>}
             </ul>
             
@@ -32,8 +53,8 @@ function State(props){
             
             <ul>
             {props.location.state.volunteer 
-                    ? props.location.state.volunteer.map(day =>{
-                        return <li>{day}</li>})
+                    ? props.location.state.volunteer.map((day, i) =>{
+                        return <li key={i}>{day}</li>})
                         : <p>Coming Soon...</p>}
             </ul>
         
@@ -47,8 +68,6 @@ function State(props){
         </div>
 
     )
-
-
 }
 
 
